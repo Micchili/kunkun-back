161be95b5626b6617758d4d14117389f2a303d64
Micchili <wot.dannbo@gmail.com>
2020-11-02 15:24:23 +0900

    if文が機能してなかったので改良

--- app.js
+++ app.js
@@ -36,28 +36,30 @@ app.get('/post', (request, response) => {
     var faceData
     const subscriptionKey = process.env.API_KEY
     const endpoint = process.env.API_URL
-    const imageUrl = request.body.image
 
     console.log(imageUrl)
 
-    if (imageUrl) response.status("400").send("何も送られていません")
-
-    axios({
-        method: 'post',
-        url: endpoint,
-        params: {
-            returnFaceAttributes: 'emotion'
-        },
-        data: {
-            url: imageUrl,
-        },
-        headers: { 'Ocp-Apim-Subscription-Key': subscriptionKey }
-    }).then((res) => {
-        res.data.forEach((face) => {
-            faceData = JSON.stringify(face.faceAttributes.emotion)
-            response.status(200).send(faceData)
+    if (request.body.image){ response.status("400").send("何も送られていません")}
+    else { 
+        const imageUrl = request.body.image
+        console.log(imageUrl)
+        axios({
+            method: 'post',
+            url: endpoint,
+            params: {
+                returnFaceAttributes: 'emotion'
+            },
+            data: {
+                url: imageUrl,
+            },
+            headers: { 'Ocp-Apim-Subscription-Key': subscriptionKey }
+        }).then((res) => {
+            res.data.forEach((face) => {
+                faceData = JSON.stringify(face.faceAttributes.emotion)
+                response.status(200).send(faceData)
+            });
+        }).catch((error) => {
+            console.log(error)
         });
-    }).catch((error) => {
-        console.log(error)
-    });
+    }
 });
